c7f6f41d34c1ce5ddad45d23030ed909cb966554
GatinhoGordo <dbarbosa15987@gmail.com>
2022-01-20 18:17:58 +0000

    getUser, autenticaUser, criarConta, checkUserName, alterarPassword feitos, ainda por testar

--- src/ServerWorker.java
+++ src/ServerWorker.java
@@ -2,10 +2,7 @@ import netscape.javascript.JSObject;
 
 import java.io.*;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 public class ServerWorker implements Runnable{
@@ -18,7 +15,7 @@ public class ServerWorker implements Runnable{
         this.s=s;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.c = DriverManager.getConnection("jdbc:mysql://192.168.0.107:3306/mydb", "afonso", "bomdia123");
+            this.c = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "bomdia123");
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
@@ -35,40 +32,178 @@ public class ServerWorker implements Runnable{
             
             DataInputStream in = new DataInputStream(new BufferedInputStream(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
-            String a = in.readUTF();
-            System.out.println(a);
+            String query = in.readUTF();
+            System.out.println(query);
+            ResultSet rs;
+            java.sql.Statement statement = c.createStatement();
 
-            switch (a) {
+            /*
+            Queries:
 
+            -> getUser (tudo)
+            -> autenticaUser OK
+            -> criarConta OK
+            -> checkUserName OK
+            -> alterarPassword OK
+            -> getLoja (tudo)
+            -> getLojaPreview
+            -> getCategorias
 
-                case "getUser":
-                    String usern = in.readUTF();
-                    System.out.println(usern);
-                    java.sql.Statement statement = c.createStatement();
-                    ResultSet rs = statement.executeQuery("SELECT * FROM "+ "utilizador" +" WHERE `username`=\""+usern+"\";");
-                    while(rs.next()){
-                        String username = rs.getString("username");
-                        String nome = rs.getString("nomeCompleto");
-                        String password = rs.getString("password");
-                        String morada = rs.getString("morada");
-                        String email = rs.getString("email");
+             */
 
-                        User user = new User(username,nome,email,password,morada);
-                        ObjectOutputStream o = new ObjectOutputStream(out);
-                        o.writeObject(user);o.flush();
-                        out.flush();
+            switch (query) {
+                //isto vai acontecer apenas quando o user visita o seu perfil
+                case "getUser" -> {
 
-                        
+                    //Como esta query só vai ser usada quando o user já estiver autenticado, o username já estará em cache
+                    String username = in.readUTF();
+                    System.out.println(username);
+
+                    //Obter a informação do user
+                    rs = statement.executeQuery("SELECT `email`,`nomeCompleto`,`morada` FROM utilizador WHERE `username`='" + username + "';");
+
+                    //Enviar a informação do user
+                    out.writeUTF(rs.getString("email"));
+                    out.writeUTF(rs.getString("nomeCompleto"));
+                    out.writeUTF(rs.getString("morada"));
+
+                    //Obter a informação dos favoritos
+                    rs = statement.executeQuery("SELECT * FROM favorito WHERE `username`='" + username + "';");
+
+                    //Enviar a informação dos favortios
+                    out.writeInt(rs.getFetchSize());
+                    while(rs.next()) {
+                        out.writeUTF(rs.getString("idLoja"));
                     }
 
-                    break;
+                    //Obter a informação dos votos
+                    rs = statement.executeQuery("SELECT * FROM voto WHERE `utilizador_username`='" + username + "';");
 
-                
+                    //Enviar a informação dos votos
+                    out.writeInt(rs.getFetchSize());
+                    while(rs.next()){
+                        out.writeUTF(rs.getString("categoria_nomeCategoria"));
+                        out.writeUTF(rs.getString("loja_idloja"));
+                        out.writeBoolean(rs.getBoolean("voto"));
+                    }
+
+                    //Obter a informação dos comentários
+                    rs = statement.executeQuery("SELECT * FROM comentario WHERE `username`='" + username + "';");
+
+                    //Enviar informação os comentários
+                    out.writeInt(rs.getFetchSize());
+                    while(rs.next()){
+                        out.writeUTF(rs.getString("idLoja"));
+                        out.writeUTF(rs.getString("comentarioText"));
+                        var timestamp = rs.getTimestamp("data");
+                        out.writeLong(timestamp.getTime());
+
+                    }
+
+                    out.flush();
+                }
+
+                //quando um user é auteticado apenas é enviado uma confirmação e o seu username
+                case "autenticaUser" -> {
+
+                    //Receber username e pass que o user introduz
+                    String usernameInput = in.readUTF();
+                    String passwordInput = in.readUTF();
 
 
-            
-                default:
-                    break;
+                    rs = statement.executeQuery("SELECT `username` FROM utilizador WHERE `username`='" + usernameInput +"' AND `password`='" + passwordInput + "';");
+
+                    boolean autenticado = rs.getFetchSize()!=0;
+                    out.writeBoolean(autenticado);
+                    out.flush();
+
+                    //User autenticado, enviar agora o username
+                    if(autenticado){
+                        out.writeUTF(rs.getString("username"));
+                        out.flush();
+                    }
+
+                }
+
+
+                case "criarConta" -> {
+
+                    String usernameInput = in.readUTF();
+                    String passwordInput = in.readUTF();
+                    String nomeCompletoInput = in.readUTF();
+                    String moradaInput = in.readUTF();
+                    String emailInput = in.readUTF();
+
+
+
+                    //Verificar se o userName está disponível
+                    rs = statement.executeQuery("SELECT * FROM utilizador WHERE `username`='" + usernameInput + "';");
+                    boolean userNameDisponivel = rs.getFetchSize()==0;
+                    out.writeBoolean(userNameDisponivel);
+
+                    //Verificar se o email está disponível
+                    rs = statement.executeQuery("SELECT * FROM utilizador WHERE `email`='" + emailInput + "';");
+                    boolean emailDisponivel = rs.getFetchSize()==0;
+                    out.writeBoolean(emailDisponivel);
+                    out.flush();
+
+                    //Credenciais confirmadas, inserir User na db
+                    if(userNameDisponivel&&emailDisponivel) {
+                        rs = statement.executeQuery("INSERT INTO utilizador VALUES (" + usernameInput +"," + emailInput + "," + passwordInput + "," + nomeCompletoInput + "," + moradaInput + ")");
+                    }
+
+                }
+
+                case "checkUserName" -> {
+
+                    //Receber username que o user introduz
+                    String usernameInput = in.readUTF();
+
+                    rs = statement.executeQuery("SELECT * FROM utilizador WHERE `username`='" + usernameInput + "';");
+
+                    //username é único, por isso se não existir na base de dados, está disponível
+                    boolean userNameDisponivel = rs.getFetchSize()==0;
+                    out.writeBoolean(userNameDisponivel);
+                    out.flush();
+
+                }
+
+                case "alterarPassword" -> {
+
+                    //Receber o username, a antiga pass e a nova
+                    String username = in.readUTF();
+                    String oldPasswordInput = in.readUTF();
+                    String newpasswordInput = in.readUTF();
+
+                    boolean passIgual = oldPasswordInput.equals(newpasswordInput);
+                    boolean passCerta=false;
+
+                    rs = statement.executeQuery("SELECT `password` FROM utilizador WHERE `username`='" + username + "';");
+                    if(rs.getString(0).equals(oldPasswordInput)){
+                        passCerta=true;
+                    }
+
+                    //Alterar a pass, update da db
+                    if(!passIgual&&passCerta) {
+                        rs = statement.executeQuery("UPDATE utilizador SET `password`='" + newpasswordInput + "' WHERE `username`='" + username + "';");
+                    }
+
+                    out.writeBoolean(passIgual);
+                    out.writeBoolean(passCerta);
+                    out.flush();
+
+                }
+
+                case "getLoja" -> {
+
+
+
+
+
+                }
+
+                default -> {
+                }
             }
 
 
